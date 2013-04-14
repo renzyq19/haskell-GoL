632f9bcb6cba5c5fc64198e79ceadc4a26a9f9cf
Will de Renzy-Martin <wrd11@doc.ic.ac.uk>
2013-04-14 16:48:21 +0100

    separating into modules

---
+++ Coordinate.hs
@@ -0,0 +1,6 @@
+module Coordinate where
+
+data Coord = Coord {x :: Int, y :: Int} 
+                    deriving(Eq,Ord, Show)
+
+
--- Main.hs
+++ Main.hs
@@ -1,6 +1,7 @@
 import System.IO
 import Data.List
 import Control.Monad
+import Coordinate
 
 data Cell = Alive | Dead
             deriving(Ord,Eq)
@@ -9,14 +10,12 @@ instance Show Cell where
     show Alive = "A"
     show Dead  = " "
 
-data Coordinate = Coordinate {x :: Int, y :: Int} 
-                    deriving(Eq,Ord, Show)
 
 type Board = [[Cell]]
 
 
 data GameState = GameState {
-    aliveCells :: [Coordinate],
+    aliveCells :: [Coord],
     rate       :: Int
 }
 
@@ -50,35 +49,35 @@ showBoard (GameState alives _ ) = display
         [[alive x y | x <- [0..boardWidth] ] | y <- [0..boardHeight]]
         where
             alive x y 
-                | Coordinate x y `elem` alives = Alive
+                | Coord x y `elem` alives = Alive
                 | otherwise                    = Dead
 
 crank :: GameState -> GameState
 crank state@(GameState alives rate) = (GameState newAlives rate)
     where 
         newAlives = 
-            [Coordinate x y | x <- [0..boardWidth],y <-[0..boardHeight],
-                                    liveOn (Coordinate x y) state]
+            [Coord x y | x <- [0..boardWidth],y <-[0..boardHeight],
+                                    liveOn (Coord x y) state]
 toString :: Board -> [String]
 toString board = [show row | row <- board]
 
 display :: Board -> IO ()
 display = (mapM_ putStrLn).toString
 
-liveOn :: Coordinate -> GameState -> Bool
+liveOn :: Coord -> GameState -> Bool
 liveOn coord (GameState alives _) = 
     (coord `elem` alives && aliveNeighbours coord == 2 )
     || aliveNeighbours coord == 3
     where
     aliveNeighbours = length.(filter (`elem` alives)).neighbours 
 
-neighbours :: Coordinate -> [Coordinate]
-neighbours (Coordinate x y) = 
-                [Coordinate i j| i <- (pm x), j <- (pm y), (x /=i || y /=j),
+neighbours :: Coord -> [Coord]
+neighbours (Coord x y) = 
+                [Coord i j| i <- (pm x), j <- (pm y), (x /=i || y /=j),
                                 onBoard i, onBoard j]
             where 
                 pm z = map (+z) [-1,0,1]
                 onBoard m = 0 <= m && m <= boardWidth
 
-resurrect :: Coordinate -> GameState -> GameState
+resurrect :: Coord -> GameState -> GameState
 resurrect (cell) (GameState alives rate) = GameState (cell : alives) rate 
